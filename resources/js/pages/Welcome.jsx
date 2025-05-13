a005fbee16184bdf9ab23120295e3761e267e323
jihan-p <jihan.pahlevi@smkn-2sbg.sch.id>
2025-05-13 17:50:15 +0700

    docs: Tambahkan atribusi pengembang ke footer Welcome page
    Menambahkan baris atribusi pengembang kustom ke bagian copyright footer pada halaman Welcome.
    
    Perubahan ini dilakukan sesuai dengan lisensi template pihak ketiga (CC BY 3.0) yang mengizinkan penambahan atribusi baru di samping atribusi asli kepada Colorlib.

--- resources/js/pages/Welcome.jsx
+++ resources/js/pages/Welcome.jsx
@@ -772,6 +772,9 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                     <p>
                                         {/* Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. */}
                                         Copyright &copy;{new Date().getFullYear()} All rights reserved | This template is made with <i className="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
+                                        {/* === Tambahkan kontribusi Anda di bawah sini === */}
+                                        <br/> {/* Opsional: Pindah baris */}
+                                        Adapted and developed by <a href="https://t.me/jhanplv" target="_blank" >jipi</a> @RPL SMKN 2 Subang {/* Ganti dengan teks kontribusi Anda */}
                                         {/* Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. */}
                                     </p>
                                 </div>
